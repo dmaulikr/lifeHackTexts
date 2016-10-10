2035aef227b70bd295a7af6f1ac669e1cd5deaeb
Samuel Rodriguez <surfer_samu@hotmail.com>
2016-10-09 20:13:04 -0400

    commit - 1

--- lifeHackTexts/ViewController.swift
+++ lifeHackTexts/ViewController.swift
@@ -7,13 +7,62 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Request Permission
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {(granted, error) in
+        
+            if granted{
+                print("Notification access granted")
+            }else{
+                print(error?.localizedDescription)
+            }
+        
+        })
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func notifyButtonTapped(sender: UIButton){
+        scheduledNotification(inSeconds: 5, completion: { success in
+            if success{
+                print("Succesfully scheduled notification")
+            }else{
+                print("Error scheduling notifcation")
+            }})
+        }
+    
+    func scheduledNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+//    func scheduledNotification(inSeconds: TimeInterval, completion: (Success: Bool) -> ()) {
+        
+        let notif = UNMutableNotificationContent()
+        notif.title = "New Notification"
+        notif.subtitle = "These are great"
+        notif.body = "The new notification options in IOS 10 are what I've always dreamed of!"
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: {error in
+            if error != nil {
+                print(error)
+                completion(false)
+            }else{
+                completion(true)
+            }
+        })
+        
+        
+        
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
